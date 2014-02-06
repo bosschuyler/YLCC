6a8f40c516bccd42c38795daa24471a30d39f0a3
bosschuyler <schuyler.bos@gmail.com>
2014-02-06 02:13:28 -0500

    db

--- app/config/database.php
+++ app/config/database.php
@@ -47,14 +47,13 @@ return array(
 	'connections' => array(
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'db',
-			'username'  => 'user',
-			'password'  => 'pass',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-
+ 			'host'      => 'localhost',
+ 			'database'  => 'ylccc_main',
+ 			'username'  => 'ylccc_main',
+ 			'password'  => 'C0asters',
+ 			'charset'   => 'utf8',
+ 			'collation' => 'utf8_unicode_ci',
+ 			'prefix'    => '',
 		),
 	),
 
